46b221152d5ce591bcd796806d6b99825a8b2fa1
Allan <allan@mertner.com>
2025-07-10 17:51:50 +0100

    Make the main window 90% opaque

--- data.lua
+++ data.lua
@@ -12,7 +12,7 @@ styles.botsgui_frame_style = {
     priority = "extra-high-no-scale",
     corner_size = { 3, 3 },
     position = { 0, 0 },
-    opacity = 0.5     -- 0 is fully transparent, 1 is fully opaque
+    opacity = 0.9     -- 0 is fully transparent, 1 is fully opaque
   }
 }
 
