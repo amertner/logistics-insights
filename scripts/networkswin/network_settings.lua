c12972c2f439a1bf57390ab217237f3884eb003a
Allan <allan@mertner.com>
2025-09-30 22:57:48 +0100

    Added individual revert controls for each per-network setting. Unified the settings so the big Clear button is just a revert button.

--- scripts/networkswin/network_settings.lua
+++ scripts/networkswin/network_settings.lua
@@ -8,18 +8,21 @@ local network_data = require "scripts.network-data"
 local WINDOW_NAME = "li_network_settings_window"
 local WINDOW_MIN_HEIGHT = 110-3*24 
 local WINDOW_MAX_HEIGHT = 110+10*24 
-local clear_mismatched_storage_action="clear-mismatch-storage-list"
-local clear_undersupply_ignore_list_action="clear-undersupply-ignore-list"
+local clear_mismatched_storage_action="chests-on-ignore-list"
+local clear_undersupply_ignore_list_action="items-on-undersupply-ignore-list"
 local ignore_higher_quality_matches_setting="ignore-higher-quality-mismatches"
 local ignore_buffer_chests_setting="ignore-buffer-chests"
-local revert_to_defaults_button_name="li_network_settings_revert_to_defaults"
+local revert_to_defaults_button_name="network-settings-revert-to-defaults"
+
+---@class NetworkSettingControls
+---@field revert LuaGuiElement The revert button for this setting
+---@field control LuaGuiElement The main control (checkbox, button, etc)
+---@field default any The default value for this setting
 
 -- Add a Network ID header line
 ---@param ui LuaGuiElement The parent UI element to add the header to
 ---@param player_table PlayerData The player's data table
 local function add_network_id_header(ui, player_table)
-  if not ui or not ui.valid then return nil end
-
   local header = ui.add{type="label", caption={"network-settings.window-title", 0}, style="bold_label"}
   header.style.left_margin = 4
   header.style.top_margin = 4
@@ -27,76 +30,71 @@ local function add_network_id_header(ui, player_table)
 end
 
 -- Add a header line to indicate a section of settings beginning
----@returns LuaGuiElement|nil
+---@returns LuaGuiElement Returns the table that will contain the settings
 local function add_settings_header(ui, caption)
-  if not ui or not ui.valid then return nil end
+  ui.add{type="label", caption=caption, style="caption_label"}
+  local settings_table = ui.add{type="table", column_count=2}
+  return settings_table
+end
 
-  local header = ui.add{type="label", caption=caption, style="caption_label"}
-  return header
+-- Add a flow containing a revert button and a label with a tooltip
+---@param table LuaGuiElement The parent UI element to add the header to
+---@param setting_name string The name of the setting (used for localization keys and element names)
+---@returns LuaGuiElement The revert button created
+local function add_label_with_revert_button(table, setting_name)
+  local hflow = table.add{type="flow", direction="horizontal"}
+  local revert_button = hflow.add {type="sprite-button", style="mini_tool_button_red", sprite="utility/reset_white", tooltip={"network-settings.setting-has-default-value"}, tags={name=setting_name, action="revert"}}
+  revert_button.enabled = false
+  hflow.add{type="label", style="label", caption={"network-settings."..setting_name}, tooltip={"network-settings."..setting_name.."-tooltip"}}
+  return revert_button
 end
 
 -- Add a checkbox setting
----@param ui LuaGuiElement The parent UI element to add the header to
+---@param table LuaGuiElement The parent UI element to add the header to
 ---@param setting_name string The name of the setting (used for localization keys and element names)
 ---@param default_state boolean The default state of the checkbox
-local function add_checkbox_setting(ui, setting_name, default_state)
-  if not ui or not ui.valid or not setting_name then return end
-
-  local checkbox = ui.add{type="checkbox", name="li_"..setting_name, 
-    caption={"network-settings."..setting_name}, tooltip={"network-settings."..setting_name.."-tooltip"}, tags={action=setting_name}, state=default_state}
-  return checkbox
+---@returns NetworkSetting
+local function add_checkbox_setting(table, setting_name, default_state)
+  local revert = add_label_with_revert_button(table, setting_name)
+  local checkbox = table.add{type="checkbox", name="li_"..setting_name, tags={name=setting_name, action="set"}, state=default_state}
+  return {revert = revert, control = checkbox, default=default_state}
 end
 
 -- Add a setting that has a text label and a Clear button, referring to a list of things
----@param ui LuaGuiElement The parent UI element to add the setting to
----@param caption LocalisedString The caption for the setting
----@param action_name string The action tag to associate with the Clear button
--- Add a setting that has a descriptive name and a Clear button
-local function add_setting_with_clear_button(ui, caption, action_name)
-  if not ui or not ui.valid or not caption then return end
+---@param table LuaGuiElement The parent table element to add the setting to
+---@param setting_name string The caption for the setting
+---@returns NetworkSetting
+local function add_setting_with_list(table, setting_name)
+  local revert = add_label_with_revert_button(table, setting_name)
+  local label = table.add{type="label", style="label", caption="0"}
 
-  local setting_flow = ui.add{type="flow", direction="horizontal"}
-  setting_flow.style.horizontal_spacing = 8
-  local label = setting_flow.add{type="label", style="label", name=action_name.."-label", caption={caption, 0}, tooltip={caption.."-tooltip"}, tags={caption_key=caption}}
-  label.style.top_margin = 4
-  local space = setting_flow.add {type = "empty-widget", style = "draggable_space"}
-  space.style.horizontally_stretchable = true
-  local button = setting_flow.add{type="button", style="other_settings_gui_button", name=action_name.."-clear", tags={action=action_name}, caption={"network-settings.clear-list-button"}}
-  button.enabled = false
-  return setting_flow
+  return {revert = revert, control = label, default=0}
 end
 
 -- Add all suggestions-related settings
 ---@param ui LuaGuiElement The parent UI element to add the settings to
 ---@param player_table PlayerData The player's data table
 local function add_suggestions_settings(ui, player_table)
-  if not ui or not ui.valid then return end
+  local settings_table = add_settings_header(ui, {"network-settings.mismatched-storage-header"})
 
-  local setting_flow = ui.add{type="flow", direction="horizontal"}
-  local vflow = setting_flow.add {type = "flow", direction = "vertical"}
+  local setting = add_checkbox_setting(settings_table, ignore_higher_quality_matches_setting, false)
+  player_table.ui.network_settings[ignore_higher_quality_matches_setting] = setting
 
-  add_settings_header(vflow, {"network-settings.mismatched-storage-header"})
-  local checkbox = add_checkbox_setting(vflow, ignore_higher_quality_matches_setting, false)
-  player_table.ui.network_settings.ignore_higher_quality_mismatches = checkbox
-  local flow = add_setting_with_clear_button(vflow, "network-settings.chests-on-ignore-list", clear_mismatched_storage_action)
-  player_table.ui.network_settings.mismatched_storage_flow = flow
+  setting = add_setting_with_list(settings_table, clear_mismatched_storage_action)
+  player_table.ui.network_settings[clear_mismatched_storage_action] = setting
 end
 
 -- Add all Undersupply-related settings
 ---@param ui LuaGuiElement The parent UI element to add the settings to
 ---@param player_table PlayerData The player's data table
 local function add_undersupply_settings(ui, player_table)
-  if not ui or not ui.valid then return end
-
-  local setting_flow = ui.add{type="flow", direction="horizontal", name="undersupply_h"}
-  local vflow = setting_flow.add {type = "flow", direction = "vertical", name="undersupply_v"}
-
-  add_settings_header(vflow, {"network-settings.undersupply-header"})
+  local settings_table = add_settings_header(ui, {"network-settings.undersupply-header"})
+  
   -- Add ignore buffer chests setting
-  local ignore_buffer_checkbox = add_checkbox_setting(vflow, ignore_buffer_chests_setting, false)
-  player_table.ui.network_settings.ignore_buffer_chests = ignore_buffer_checkbox
-  local flow = add_setting_with_clear_button(vflow, "network-settings.items-on-undersupply-ignore-list", clear_undersupply_ignore_list_action)
-  player_table.ui.network_settings.ignored_undersupply_items = flow
+  local setting = add_checkbox_setting(settings_table, ignore_buffer_chests_setting, false)
+  player_table.ui.network_settings[ignore_buffer_chests_setting] = setting
+  setting = add_setting_with_list(settings_table, clear_undersupply_ignore_list_action)
+  player_table.ui.network_settings[clear_undersupply_ignore_list_action] = setting
 end
 
 --- Create settings window
@@ -134,29 +132,54 @@ function network_settings.create_frame(parent, player)
     add_undersupply_settings(subheader_frame, player_table)
 end
 
-local function update_setting_with_clear_button(setting_flow, action_name, count)
-  if not setting_flow or not setting_flow.valid then return end
-
-  local label = setting_flow[action_name.."-label"]
-  if label and label.valid then
-    local key = label.tags and label.tags.caption_key or (label.caption and label.caption[1])
-    label.caption = { key, count }
+---@param control NetworkSettingControls
+---@param count number The number of items in the list this setting refers to
+local function update_list_setting(control, count)
+  local changed = 0
+  if count > 0 then
+    changed = 1
   end
 
-  local button = setting_flow[action_name.."-clear"]
-  if button and button.valid then
-    button.enabled = (count > 0)
+  if control.revert and control.revert.valid then
+    control.revert.enabled = changed > 0
+    if changed > 0 then
+      control.revert.tooltip = {"network-settings.reset-list-setting"}
+    else
+      control.revert.tooltip = {"network-settings.setting-has-default-value"}
+    end
   end
+  control.control.caption = tostring(count)
+  return changed
 end
 
--- Update the settings UI to reflect current settings
+---@param control NetworkSettingControls
+---@param state boolean
+---@return number Return number of settings changed (0 or 1)
+function update_checkbox_setting(control, state)
+  local changed = 0
+  if state ~= control.default then
+    changed = 1
+  end
+  if control.revert and control.revert.valid then
+    control.revert.enabled = changed > 0
+    if changed > 0 then
+      control.revert.tooltip = {"network-settings.reset-setting-to-default-tooltip", control.default}
+    else
+      control.revert.tooltip = {"network-settings.setting-has-default-value"}
+    end
+  end
+  if control.control and control.control.valid then
+    control.control.state = state
+  end
+  return changed
+end
+
+--- Update the settings UI to reflect current settings
 ---@param player? LuaPlayer
 ---@param player_table PlayerData
 function network_settings.update(player, player_table)
   if not player or not player.valid or not player_table then return end
   if not player_table.ui or not player_table.ui.network_settings then return end
-
-  local num_changed = 0 -- Number of settings that are not the default
 
   local network_id = player_table.settings_network_id
   local networkdata = network_data.get_networkdata_fromid(network_id)
@@ -168,23 +191,19 @@ function network_settings.update(player, player_table)
     header.caption = {"network-settings.window-title", network_id or 0}
   end
 
-  -- Update Suggestions settings
-  player_table.ui.network_settings.ignore_higher_quality_mismatches.state = networkdata.ignore_higher_quality_mismatches
-  local flow = player_table.ui.network_settings.mismatched_storage_flow
-  local count = networkdata and table_size(networkdata.ignored_storages_for_mismatch) or 0
-  update_setting_with_clear_button(flow, clear_mismatched_storage_action, count)
-
-  -- Update Undersupply settings
-  -- Update ignore buffer chests setting
-  if player_table.ui.network_settings.ignore_buffer_chests then
-    player_table.ui.network_settings.ignore_buffer_chests.state = networkdata.ignore_buffer_chests_for_undersupply or false
+  -- Iterate over all settings and update them
+  local num_changed = 0 -- Number of settings that are not the default
+  for name, control in pairs(player_table.ui.network_settings) do
+    if name == ignore_higher_quality_matches_setting then
+      num_changed = num_changed + update_checkbox_setting(control, networkdata.ignore_higher_quality_mismatches)
+    elseif name == ignore_buffer_chests_setting then
+      num_changed = num_changed + update_checkbox_setting(control, networkdata.ignore_buffer_chests_for_undersupply)
+    elseif name == clear_mismatched_storage_action then
+      num_changed = num_changed + update_list_setting(control, table_size(networkdata.ignored_storages_for_mismatch))
+    elseif name == clear_undersupply_ignore_list_action then
+      num_changed = num_changed + update_list_setting(control, table_size(networkdata.ignored_items_for_undersupply))
+    end
   end
-  flow = player_table.ui.network_settings.ignored_undersupply_items
-  count = networkdata and table_size(networkdata.ignored_items_for_undersupply) or 0
-  update_setting_with_clear_button(flow, clear_undersupply_ignore_list_action, count)
-
-  if networkdata.ignore_higher_quality_mismatches then num_changed = num_changed + 1 end
-  if networkdata.ignore_buffer_chests_for_undersupply then num_changed = num_changed + 1 end
   local defaults_button = player_table.ui.network_settings.defaults_button
   if defaults_button and defaults_button.valid then
     if num_changed == 0 then
@@ -197,51 +216,45 @@ function network_settings.update(player, player_table)
   end
 end
 
-local function adopt_checkbox_state(event)
-  local player_table = player_data.get_player_table(event.player_index)
-  if not player_table then return false end
-
+---@param player_table PlayerData
+---@param name string
+---@param setting NetworkSettingControls
+local function set_checkbox_setting(player_table, name, action, setting)
   local network_id = player_table.settings_network_id
   local networkdata = network_data.get_networkdata_fromid(network_id)
   if not networkdata then return false end
 
-  if event.element.tags.action == ignore_higher_quality_matches_setting then
-    networkdata.ignore_higher_quality_mismatches = player_table.ui.network_settings.ignore_higher_quality_mismatches.state
-  elseif event.element.tags.action == ignore_buffer_chests_setting then
-    networkdata.ignore_buffer_chests_for_undersupply = player_table.ui.network_settings.ignore_buffer_chests.state
+  local new_value
+  if action == "revert" then
+    new_value = setting.default
+  else
+    new_value = setting.control.state
   end
-
-  -- Update UI
-  local player = game.get_player(event.player_index)
-  network_settings.update(player, player_table)
+  if name == ignore_higher_quality_matches_setting then
+    networkdata.ignore_higher_quality_mismatches = new_value
+  elseif name == ignore_buffer_chests_setting then
+    networkdata.ignore_buffer_chests_for_undersupply = new_value
+  else
+    return false
+  end
 end
 
 -- Clear the relevant list and refresh the window to show the effect
-local function clear_list_and_refresh(event)
-  local player_table = player_data.get_player_table(event.player_index)
-  if not player_table then return false end
-
+local function clear_list_and_refresh(player_table, event)
   local network_id = player_table.settings_network_id
   local networkdata = network_data.get_networkdata_fromid(network_id)
   if not networkdata then return false end
 
-  if event.element.tags.action == clear_mismatched_storage_action then
+  if event.element.tags.name == clear_mismatched_storage_action then
     -- Clear the list of ignored storages for mismatched storage suggestion
     networkdata.ignored_storages_for_mismatch = {}
-  elseif event.element.tags.action == clear_undersupply_ignore_list_action then
+  elseif event.element.tags.name == clear_undersupply_ignore_list_action then
     -- Clear the list of ignored storages for undersupply
     networkdata.ignored_items_for_undersupply = {}
   end
-
-  -- Update UI
-  local player = game.get_player(event.player_index)
-  network_settings.update(player, player_table)
 end
 
-local function revert_to_defaults(event)
-  local player_table = player_data.get_player_table(event.player_index)
-  if not player_table then return false end
-
+local function revert_to_defaults(player_table, event)
   local network_id = player_table.settings_network_id
   local networkdata = network_data.get_networkdata_fromid(network_id)
   if not networkdata then return false end
@@ -251,26 +264,47 @@ local function revert_to_defaults(event)
     networkdata.ignore_buffer_chests_for_undersupply = false
     networkdata.ignored_storages_for_mismatch = {}
     networkdata.ignored_items_for_undersupply = {}
-    local player = game.get_player(player_table.player_index)
-    network_settings.update(player, player_table)
   end
 end
 
+--- Handle clicks
 ---@returns boolean true if the click was handled
 function network_settings.on_gui_click(event)
   if not event.element or not event.element.valid then return false end
   if not event.element.tags then return false end
 
-  if event.element.tags.action == ignore_higher_quality_matches_setting or event.element.tags.action == ignore_buffer_chests_setting then
-    adopt_checkbox_state(event)
-  elseif event.element.tags.action == clear_mismatched_storage_action or event.element.tags.action == clear_undersupply_ignore_list_action then
-    clear_list_and_refresh(event)
-  elseif event.element.name == revert_to_defaults_button_name then
-    revert_to_defaults(event)
-  else
-    return false
+  local handled = false
+  local player_table = player_data.get_player_table(event.player_index)
+  if player_table then
+    local setting_name = event.element.tags.name
+    local action = event.element.tags.action
+    local controls = player_table.ui.network_settings[setting_name]
+    if controls then
+      if controls.control and controls.control.valid then
+        if controls.control.type == "checkbox" then
+          set_checkbox_setting(player_table, setting_name, action, controls)
+          handled = true
+        end
+        if action == "revert" and controls.control.type == "label" then
+          -- Revert button for a list setting
+          if setting_name == clear_mismatched_storage_action or setting_name == clear_undersupply_ignore_list_action then
+            clear_list_and_refresh(player_table, event)
+            handled = true
+          end
+        end
+      end
+    end
+
+    if event.element.name == revert_to_defaults_button_name then
+      revert_to_defaults(player_table, event)
+      handled = true
+    end
+    if handled then
+      local player = game.get_player(player_table.player_index)
+      network_settings.update(player, player_table)      
+    end
   end
-  return true
+  return handled
 end
 
 
