f40a9a591bd6be70208a51b25408da93fe2c7f1b
Allan <allan@mertner.com>
2025-10-14 11:44:02 +0100

    Remove unused play/pause mini buttons

--- prototypes/sprite.lua
+++ prototypes/sprite.lua
@@ -1,22 +1,6 @@
 data:extend{
   {
     type = "sprite",
-    name = "li_pause",
-    filename = "__logistics-insights__/graphics/pause-button.png",
-    size = 64,
-    scale = 1.0,
-    flags = {"icon"},
-  },
-  {
-    type = "sprite",
-    name = "li_play",
-    filename = "__logistics-insights__/graphics/play-button.png",
-    size = 128,
-    scale = 1.0,
-    flags = {"icon"},
-  },
-  {
-    type = "sprite",
     name = "li_step",
     filename = "__logistics-insights__/graphics/step-button.png",
     size = 64,
--- scripts/mainwin/mini_button.lua
+++ scripts/mainwin/mini_button.lua
@@ -3,21 +3,7 @@
 local mini_button = {}
 local player_data = require("scripts.player-data")
 
----@alias ButtonType "pause" | "trash" | "networks" | "settings"
-
---- Update the start/stop button appearance based on current state
---- @param element LuaGuiElement The button element to update
---- @param is_paused boolean Whether the data gathering is currently paused
-function mini_button.update_paused(element, is_paused)
-  -- Update button appearance to reflect current state
-  if element then
-    if is_paused then
-      element.sprite = "li_play"
-    else
-      element.sprite = "li_pause"
-    end
-  end
-end
+---@alias ButtonType "trash" | "networks" | "settings"
 
 --- @param player_table PlayerData The player's data table
 ---@param name string The name of the button
@@ -46,7 +32,7 @@ end
 ---@param label_ui LuaGuiElement The parent UI element to add the button to
 ---@param button_name string The button identifier ("history", "undersupply", etc.)
 ---@param tooltip LocalisedString The tooltip identifier for the button
----@param button_type ButtonType The type of button ("pause", "trash", etc.)
+---@param button_type ButtonType The type of button ("trash", etc.)
 ---@param is_paused boolean Whether the button should start in a paused state
 function mini_button.add(player_table, label_ui, button_name, tooltip, button_type, is_paused)
   -- Add flexible spacer that pushes button to the right
@@ -58,13 +44,7 @@ function mini_button.add(player_table, label_ui, button_name, tooltip, button_ty
 
   -- Determine the sprite based on button type
   local sprite
-  if button_type == "pause" then
-    if is_paused then
-      sprite = "li_play"
-    else
-      sprite = "li_pause"
-    end
-  elseif button_type == "trash" then
+  if button_type == "trash" then
     sprite = "utility/trash"
   elseif button_type == "networks" then
     sprite = "li_list"
